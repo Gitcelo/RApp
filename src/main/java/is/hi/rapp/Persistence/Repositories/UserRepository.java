28083576ce58b57f70b7b6f1a41d4e70dbd02219
Gitcelo <mfa5@hi.is>
2021-10-26 14:38:35 +0000

    UserRepo fikt

--- src/main/java/is/hi/rapp/Persistence/Repositories/UserRepository.java
+++ src/main/java/is/hi/rapp/Persistence/Repositories/UserRepository.java
@@ -1,4 +1,17 @@
 package is.hi.rapp.Persistence.Repositories;
 
-public interface UserRepository {
+import is.hi.rapp.Persistence.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository {
+    User save(User user);
+    String delete(User user); //We return a string instead of nothing to be more RESTful
+    @SQL
+    String changePassword(User user, String password, String newPassword);
+
+    List<User> findAll();
+    List<User> findByTitle(String title);
+    User findByID(long id);
 }
