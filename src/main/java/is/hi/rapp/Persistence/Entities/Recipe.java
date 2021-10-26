7ee473417e06232b7f08d91839dfbdce754bf0ba
stein <steinunngretak@gmail.com>
2021-10-26 12:20:28 +0000

    Bæta við í recipe entity

--- src/main/java/is/hi/rapp/Persistence/Entities/Recipe.java
+++ src/main/java/is/hi/rapp/Persistence/Entities/Recipe.java
@@ -1,4 +1,12 @@
 package is.hi.rapp.Persistence.Entities;
+import java.util.List;
 
 public class Recipe {
+    private Long id;
+    private String title;
+    private String description;
+    private List<String> ingredients;
+    private Boolean published;
+    private List<Review> reviews;
+    private User user;
 }
