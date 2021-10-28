1219437f54fe0239496a9e79c76e5c256c30b2ec
MarzukIngi <mil4@hi.is>
2021-10-28 15:46:12 +0000

    Reyndi að útfæra custom query

--- src/main/java/is/hi/rapp/Controllers/UserControllers/UserProfileController.java
+++ src/main/java/is/hi/rapp/Controllers/UserControllers/UserProfileController.java
@@ -1,9 +1,10 @@
-package is.hi.rapp.Controllers.UserControllers;
+package is.hi.rapp.Controllers.userControllers;
 
 import is.hi.rapp.Persistence.Entities.User;
 import is.hi.rapp.Services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class UserProfileController {
     private UserService userService;
 
-    @Autowired
-    public UserProfileController(UserService userService) {
-        this.userService = userService;
+    @RequestMapping("/User/{id}", method = RequestMethod.GET)
+    public String userProfileViewGet(@PathVariable("id") long id, Model model) {
+        User userToView =  userService.findOne(id);
+
+        model.addAttribute("user", userToView)
     }
 }
--- src/main/java/is/hi/rapp/Services/UserService.java
+++ src/main/java/is/hi/rapp/Services/UserService.java
@@ -5,7 +5,9 @@ import is.hi.rapp.Persistence.Entities.User;
 import java.util.List;
 
 public interface UserService {
-    User login(String userName, String password);
+    User logIn(String userName, String password);
+    String changePassword(User user, String password, String newPassword);
+    String changeEmail(User user, String newEmail);
     User save(User user);
     void delete(User user); //We return a string instead of nothing to be more RESTful
     List<User> findAll();
