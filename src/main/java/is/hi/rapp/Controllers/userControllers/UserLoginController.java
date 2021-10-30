aab6cdf53c28aeaf33233b064deedd5f99feadcc
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2021-10-30 09:30:57 +0000

    Byrjaður á login controller

--- src/main/java/is/hi/rapp/Controllers/userControllers/UserLoginController.java
+++ src/main/java/is/hi/rapp/Controllers/userControllers/UserLoginController.java
@@ -1,8 +1,15 @@
 package is.hi.rapp.Controllers.userControllers;
 
+import is.hi.rapp.Persistence.Entities.User;
 import is.hi.rapp.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserLoginController {
@@ -11,5 +18,22 @@ public class UserLoginController {
     @Autowired
     public UserLoginController(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGET(User user) {
+        return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String loginPOST(User user, BindingResult result, Model model, HttpSession session) {
+        if(result.hasErrors()) return "login";
+        User exists = userService.login(user);
+        if(exists != null) {
+            session.setAttribute("LoggedInUser", exists);
+            model.addAttribute("LoggedInUser", exists);
+            return "LoggedInUser";
+        }
+        return "redirect:/"
     }
 }
