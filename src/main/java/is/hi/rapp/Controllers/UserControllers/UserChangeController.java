656ffa23b355e2b5d89f39e8ef00d9e3337287b0
MarzukIngi <mil4@hi.is>
2021-11-10 13:44:15 +0000

    tengdi template-in

--- src/main/java/is/hi/rapp/Controllers/UserControllers/UserChangeController.java
+++ src/main/java/is/hi/rapp/Controllers/UserControllers/UserChangeController.java
@@ -1,13 +1,31 @@
 package is.hi.rapp.Controllers.UserControllers;
 
+import is.hi.rapp.Persistence.Entities.User;
 import is.hi.rapp.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class UserChangeController {
     private UserService userService;
 
     @Autowired
     public UserChangeController(UserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping(value = "/LoggedInUser", method = RequestMethod.PATCH)
+    public String UserChangeViewPatch(User user, String password, String newPassword, String newEmail, Model model) {
+        User userToChange = userService.findByID(user.getID());
+
+        if(password.equals(userToChange.getPassword())) {
+            userToChange.setPassword(newPassword);
+            (newEmail != null)? userToChange.setEmail(newEmail) : ;
+        }
+
+        return "LoggedInUser";
     }
 }
