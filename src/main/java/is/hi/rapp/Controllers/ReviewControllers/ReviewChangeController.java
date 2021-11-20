c99e91d67e94cd78e9a1b8848b2e3390b94bfbd5
stein <steinunngretak@gmail.com>
2021-11-20 15:35:24 +0000

    Review change controller, bætti inn reviewchangeviewdelete og patch

--- src/main/java/is/hi/rapp/Controllers/ReviewControllers/ReviewChangeController.java
+++ src/main/java/is/hi/rapp/Controllers/ReviewControllers/ReviewChangeController.java
@@ -3,6 +3,12 @@ package is.hi.rapp.Controllers.ReviewControllers;
 import is.hi.rapp.Services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ReviewChangeController {
@@ -11,5 +17,17 @@ public class ReviewChangeController {
     @Autowired
     public ReviewChangeController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+    @RequestMapping(value = "/Recipe/{id}/Review/{rid}", method = RequestMethod.DELETE)
+    public String reviewChangeViewDelete(@PathVariable("id") long id, HttpSession session, Model model) {
+
+
+        return "redirect:/Recipe/"+id;
+    }
+    @RequestMapping(value = "/Recipe/{id}/Review/{rid}", method = RequestMethod.POST)
+    public String reviewChangeViewPatch(@PathVariable("id") long id, HttpSession session, Model model) {
+
+
+        return "redirect:/Recipe/"+id;
     }
 }
