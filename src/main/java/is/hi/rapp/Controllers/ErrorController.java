58c2af6f169523cb3bfac4448d9fc9d356dab3e2
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2021-11-23 23:21:50 +0000

    Stílar fyrir error komnir

--- src/main/java/is/hi/rapp/Controllers/ErrorController.java
+++ src/main/java/is/hi/rapp/Controllers/ErrorController.java
@@ -1,9 +1,13 @@
 package is.hi.rapp.Controllers;
 
+import is.hi.rapp.Persistence.Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ErrorController {
@@ -11,7 +15,9 @@ public class ErrorController {
     public ErrorController() {}
 
     @RequestMapping(value = "/error", method = RequestMethod.GET)
-    public String errorViewGet() {
+    public String errorViewGet(HttpSession session, Model model) {
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        model.addAttribute("LoggedInUser", sessionUser);
         return "error";
     }
 }
