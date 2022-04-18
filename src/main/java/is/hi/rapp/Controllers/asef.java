636bc744b5e995eb581c0a7b8076827d78dc8184
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2022-04-18 08:26:23 +0000

    Vil láta heroku redeploya

---
+++ src/main/java/is/hi/rapp/Controllers/asef.java
@@ -0,0 +1,17 @@
+package is.hi.rapp.Controllers;
+
+public class asef {
+    private String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public asef(String s) {
+        this.s = s;
+    }
+}
